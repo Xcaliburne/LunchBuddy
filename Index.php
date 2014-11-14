2cca84256e0e280f33f41f15834e8202ec080fe7
Xcaliburne <lu.gindre@gmail.com>
2014-11-14 16:05:21 +0100

    Création de la page de connexion et modification du .gitignore

---
+++ Index.php
@@ -0,0 +1,46 @@
+<!--login modal-->
+<head>
+    <title>LunchBuddy</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.css" rel="stylesheet">
+    <style>
+        .modal-footer{
+            border-top: 0px;
+        }
+    </style>
+</head>
+<body>
+    <section id="loginModal" class="modal show" tabindex="-1" role="dialog" aria-hidden="true">
+        <section class="modal-dialog">
+            <section class="modal-content">
+                <header class="modal-header">
+                    <h1 class="text-center">Login</h1>
+                </header>
+                <section class="modal-body">
+                    <form class="form col-md-12">
+                        <section class="form-group">
+                            <input class="form-control input-lg" placeholder="Pseudo" type="text">
+                        </section>
+                        <section class="form-group">
+                            <input class="form-control input-lg" placeholder="Password" type="password">
+                        </section>
+                        <section class="form-group">
+                            <button class="btn btn-primary btn-lg btn-block">Sign In</button>
+                            <span class="pull-right"><a href="#">Register</a></span><span><a href="#">Need help?</a></span>
+                        </section>
+                    </form>
+                </section>
+                <footer class="modal-footer">
+                    <section class="col-md-12">
+                        <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
+                    </section>
+                </footer>	
+            </section>
+        </section>
+    </section>
+</section>
+</body>
