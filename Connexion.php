4b978c0d28fbd7e368ddda05af6f174133a1e807
Xcaliburne <lu.gindre@gmail.com>
2014-11-21 15:38:48 +0100

    refactoring du code php

---
+++ Connexion.php
@@ -0,0 +1,66 @@
+<?php
+if (isset($_REQUEST['Connexion'])) {
+    if ((!empty($_REQUEST['Email'])) && (!empty($_REQUEST['Password']))) {
+        include_once 'personnesdb.php';
+        $email = $_REQUEST['Email'];
+        $password = $_REQUEST['Password'];
+        $erreur = '';
+        $infosPersonnes = lirePersonneConnectee($email, $password);
+        if (!$infosPersonnes) {
+            $erreur = 'Mot de passe ou Pseudo incorrecte';
+        } else {
+            $_SESSION["idPersonne"] = $infosPersonnes["idPersonne"];
+            $_SESSION["nom"] = $infosPersonnes["Nom"];
+            $_SESSION["prenom"] = $infosPersonnes["Prenom"];
+            $_SESSION["email"] = $email;
+            header('Location: index.php');
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<head>
+    <title>LunchBuddy</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.css" rel="stylesheet">
+    <style>
+        .modal-footer{
+            border-top: 0px;
+        }
+    </style>
+</head>
+<body>
+    <section id="loginModal" class="modal show" tabindex="-1" role="dialog" aria-hidden="true">
+        <section class="modal-dialog">
+            <section class="modal-content">
+                <header class="modal-header">
+                    <h1 class="text-center">Login</h1>
+                </header>
+                <section class="modal-body">
+                    <form class="form col-md-12" method="post" action="connexion.php">
+                        <section class="form-group">
+                            <input name="Email" class="form-control input-lg" placeholder="Email" type="Email">
+                        </section>
+                        <section class="form-group">
+                            <input name="Password" class="form-control input-lg" placeholder="Password" type="password">
+                        </section>
+                        <section class="form-group">
+                            <button name="Connexion" class="btn btn-primary btn-lg btn-block">Sign In</button>
+                            <span class="pull-left"><a href="#">Register</a></span>
+                        </section>
+                    </form>
+                </section>
+                <footer class="modal-footer">
+                    <section class="col-md-12">
+                        <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
+                    </section>
+                </footer>	
+            </section>
+        </section>
+    </section>
+</section>
+</body>
--- Index.php
+++ Index.php
@@ -1,46 +1,10 @@
-<!--login modal-->
-<head>
-    <title>LunchBuddy</title>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php
+if (!isset($_SESSION)){
+    session_start();
+}
+if ((empty($_SESSION["idPersonne"])) && (empty($_SESSION["nom"])) && (empty($_SESSION["prenom"])) && (empty($_SESSION["email"]))){
+    include_once 'Connexion.php';
+}  else {
+    echo 'hello world';
 
-    <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
-    <link href="css/bootstrap-theme.css" rel="stylesheet" type="text/css"/>
-    <link href="css/source.css" rel="stylesheet" type="text/css"/>
-
-    <style>
-        
-    </style>
-</head>
-<body>
-    <section id="loginModal" class="modal show" tabindex="-1" role="dialog" aria-hidden="true">
-        <section class="modal-dialog">
-            <section class="modal-content">
-                <header class="modal-header">
-                    <h1 class="text-center">Login</h1>
-                </header>
-                <section class="modal-body">
-                    <form class="form col-md-12">
-                        <section class="form-group">
-                            <input class="form-control input-lg" placeholder="Pseudo" type="text">
-                        </section>
-                        <section class="form-group">
-                            <input class="form-control input-lg" placeholder="Password" type="password">
-                        </section>
-                        <section class="form-group">
-                            <button class="btn btn-primary btn-lg btn-block">Sign In</button>
-                            <span class="pull-right"><a href="#">Register</a></span><span><a href="#">Need help?</a></span>
-                        </section>
-                    </form>
-                </section>
-                <footer class="modal-footer">
-                    <section class="col-md-12">
-                        <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
-                    </section>
-                </footer>	
-            </section>
-        </section>
-    </section>
-</section>
-</body>
+}
